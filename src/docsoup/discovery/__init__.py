12340c09ba4b6f25d7044673b883c67ce63fa129
Anugerah Erlaut <aerlaut@live.com>
2026-03-05 19:22:50 +0100

    chore: export PythonDiscoverer and PythonExtractor from package __init__ modules

--- src/docsoup/discovery/__init__.py
+++ src/docsoup/discovery/__init__.py
@@ -1,5 +1,7 @@
 """Dependency discovery implementations."""
 
 from docsoup.discovery.base import DependencyDiscoverer
+from docsoup.discovery.node import NodeDiscoverer
+from docsoup.discovery.python import PythonDiscoverer
 
-__all__ = ["DependencyDiscoverer"]
+__all__ = ["DependencyDiscoverer", "NodeDiscoverer", "PythonDiscoverer"]
